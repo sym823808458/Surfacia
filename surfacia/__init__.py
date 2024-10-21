8149b9ebab6b7e054e723d4a2180cd14e77c63a3
Yuming Su <43782314+sym823808458@users.noreply.github.com>
2024-10-21 14:43:55 +0800

    Update __init__.py

--- surfacia/__init__.py
+++ surfacia/__init__.py
@@ -5,6 +5,7 @@ from .extract_element_first import reorder_atoms
 from .extract_substructure import extract_substructure_main 
 from .xyz2gaussian import xyz2gaussian_main
 from .run_gaussian import run_gaussian
-from .readMultiwfn import process_txt_files
+from .readMultiwfn import process_txt_files,run_multiwfn_on_fchk_files,read_first_matches_csv
 from .machine_learning import xgb_stepwise_regression
 from .fchk2matches import fchk2matches_main
+
